4a5d39cd18b7fde8d943ce2cf8b3240e88d637e3
Colin Hemphill <hemphill.colin@gmail.com>
2017-06-22 12:22:11 -0500

    Block tabs style instead of underlined tabs.

--- index.js
+++ index.js
@@ -28,18 +28,35 @@ exports.decorateConfig = (config) => {
         border: none !important;
       }
       .header_header {
-        background: #282236 !important;
+        background: #1e1b26 !important;
+        top: 0;
+        left: 0;
+        right: 0;
+      }
+      .borderShim_nfs {
+        display: none;
+      }
+      .tabs_nav {
+        height: 38px;
+        line-height: 38px;
+      }
+      ul.tabs_list {
+        max-height: 38px;
+        margin-left: 80px;
       }
       .tab_tab {
         padding-left: 0;
-        border: 0;
-        border-bottom: 2px solid rgba(73, 56, 87, 1) !important;
-        transition: border-color .2s ease;
+        border: none;
+        border-left: 3px solid transparent;
+        background-color: #1e1b26;
+        transition: background-color .2s ease, border-color .2s ease;
       }
       .tab_active {
-        border-bottom: 2px solid rgba(176, 47, 68, 1) !important;
+        background-color: #282236 !important;
+        border-color: rgba(176, 47, 68, 1) !important;
       }
       .tab_text {
+        height: 38px;
         color: rgba(255, 255, 255, 0.5);
       }
       .tab_textActive {
@@ -48,8 +65,8 @@ exports.decorateConfig = (config) => {
       .tab_text, .tab_textActive {
         font-weight: 700;
       }
-      .tab_hasActivity, .tab_hasActivity:hover {
-        color: #ffff9c;
+      .tab_hasActivity .tab_text, .tab_hasActivity:hover .tab_text {
+        color: rgba(255, 255, 156, 0.8);
       }
     `
   });
